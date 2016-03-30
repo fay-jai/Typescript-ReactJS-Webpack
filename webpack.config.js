25dcd26f2ffe461c40b274e533f48da60f2d8c28
Willson Mock <willson.mock@gmail.com>
2016-03-30 10:19:00 -0700

    Initial working version of webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+var config = {
+    entry: [
+        "./src/app.ts"
+    ],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ["", ".ts", ".tsx", ".js"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
+
+module.exports = config;
