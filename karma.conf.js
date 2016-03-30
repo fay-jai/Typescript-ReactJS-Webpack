39b685efbfb4bff7d31e7a73e0f16428440d39c3
Willson Mock <willson.mock@gmail.com>
2016-03-30 15:08:13 -0700

    Add karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+var webpackConfig = require("./webpack.config");
+
+module.exports = function(config) {
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "chai", "sinon"],
+        files: [
+            "test/**/*.tsx"
+        ],
+        exclude: [
+        ],
+        preprocessors: {
+            "test/**/*.tsx": ["webpack"]
+        },
+        webpack: {
+            module: webpackConfig.module,
+            resolve: webpackConfig.resolve
+        },
+        reporters: ["mocha"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ["PhantomJS"],
+        singleRun: true,
+        concurrency: Infinity
+    });
+};
